bc35fb7f28d3817220823426ad7386bd1eec8be7
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-01-13 20:40:24 +0100

    Initial commit

---
+++ Instructions/Debug/UnityLogStringInstruction.cs
@@ -0,0 +1,19 @@
+﻿namespace Juce.Scripting.Instructions
+{
+    public class UnityLogStringInstruction : FlowScriptInstruction
+    {
+        public const string ValueIn = nameof(ValueIn);
+
+        public override void RegisterPorts()
+        {
+            AddInputPort<string>(ValueIn);
+        }
+
+        protected override void Execute()
+        {
+            string value = GetInputPortValue<string>(ValueIn);
+
+            UnityEngine.Debug.Log(value);
+        }
+    }
+}
